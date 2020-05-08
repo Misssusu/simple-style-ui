ce671baf2869d16a3658925cd550a7f8c38abe9e
suyuan <suyuan326@qq.com>
2020-05-08 17:03:27 +0800

    delete store

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import SUI from './entry'
-import store from "./store"
 
 Vue.config.productionTip = false;
 Vue.use(SUI);
 
 new Vue({
-  store,
   render: h => h(App),
 }).$mount('#app');
